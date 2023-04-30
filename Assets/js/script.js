7dccd51a51a43a01e8ced99a02f249a4c5bd71ee
JesseColon <jay.alexandervii@gmail.com>
2023-04-30 15:43:58 -0400

    got button to reflect city search text and added error handeling to fetch

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,29 +1,41 @@
-var searchBtn = document.getElementById("#searchBtn");
-var city = document.getElementById("city-search");
+$(function () {
+var city = $("#city-search").val();
 var APIKey = "5b7aef00fa1161dcd4e15780def2c64f";
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
-
-
 fetch(queryURL)
 .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return response.json();
+})
+.then(function (data) {
+  console.log(data);
+})
+.catch(function (error) {
+  console.error("Error fetching data:", error);
+});
 
-  console.log(city);
+console.log(city);
 
-  function cityList() { 
+  function appendList(){
 
+    var button = $("<button>")
 
+    .addClass("tile button is-info is-child notification level-item")
+    .text(city);
+  $("#aside").append(button);
 
- }
+   } 
 
-searchBtn.onclick = () => {
+// $('#searchBtn').click( function(){
+//   appendList();
+// });
 
-  console.log(city);
+$("#searchBtn").on("click", function () {
+  city = $("#city-search").val();
+  appendList();
+});
 
-}
-
+});
