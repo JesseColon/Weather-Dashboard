c1a4cdfa8ceda262bb00524c3b4bdfaa2a7deffe
JesseColon <jay.alexandervii@gmail.com>
2023-04-30 17:28:41 -0400

    got basic responce to print on webpage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,41 +1,37 @@
 $(function () {
-var city = $("#city-search").val();
-var APIKey = "5b7aef00fa1161dcd4e15780def2c64f";
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+  var forcastArea = $("#forcast");
+  var APIKey = "5b7aef00fa1161dcd4e15780def2c64f";
 
-fetch(queryURL)
-.then(function (response) {
-  if (!response.ok) {
-    throw new Error("Network response was not ok");
-  }
-  return response.json();
-})
-.then(function (data) {
-  console.log(data);
-})
-.catch(function (error) {
-  console.error("Error fetching data:", error);
-});
-
-console.log(city);
-
-  function appendList(){
-
+  function appendList(city){
     var button = $("<button>")
+      .addClass("tile button is-info is-child notification level-item")
+      .text(city);
+    $("#aside").append(button);
+  } 
 
-    .addClass("tile button is-info is-child notification level-item")
-    .text(city);
-  $("#aside").append(button);
+  $("#searchBtn").on("click", function () {
+    var city = $("#city-search").val();
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial"; 
+    fetch(queryURL)
+      .then(function (response) {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        
+        var tempDiv = $("<div>").attr("id", "temp");
+        var tempValue = data.main.temp;
+        tempDiv.text("Temperature: " + tempValue);
+        $("#forcast").append(tempDiv);
 
-   } 
-
-// $('#searchBtn').click( function(){
-//   appendList();
-// });
-
-$("#searchBtn").on("click", function () {
-  city = $("#city-search").val();
-  appendList();
+      })
+      .catch(function (error) {
+        console.error("Error fetching data:", error);
+      });
+    appendList(city);
+  });
 });
-
-});
+console.log
