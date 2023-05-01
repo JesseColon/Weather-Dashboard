92167d1c3339f793a5fe7de65ed37fe9f4c0e74b
JesseColon <jay.alexandervii@gmail.com>
2023-04-30 22:18:06 -0400

    got 5 day forcast to respond. but still having trouble with more spesific calls

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,5 @@
 $(function () {
-  var forcastArea = $("#forcast");
+  var currentCity;
   var APIKey = "5b7aef00fa1161dcd4e15780def2c64f";
 
   function appendList(city){
@@ -11,7 +11,7 @@ $(function () {
 
   $("#searchBtn").on("click", function () {
     var city = $("#city-search").val();
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial"; 
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&q=" + city + "&appid=" + APIKey + "&units=imperial"; 
     fetch(queryURL)
       .then(function (response) {
         if (!response.ok) {
@@ -22,16 +22,67 @@ $(function () {
       .then(function (data) {
         console.log(data);
         
+        var nameH2 = $("<h2>").attr("id", "name").addClass("title");
+        var nameValue = data.name;
+        nameH2.text(nameValue);
+        $("#forcast").append(nameH2);
+
+        var dt_textdiv = $("<div>").attr("id", "dt_text");
+        var dt_textValue = data.dt_text;
+        dt_textdiv.text(dt_textValue);
+        $("#forcast").append(dt_textdiv);
+
+        var weatherDiv = $("<div>").attr("id", "weather");
+        var weatherValue = data.weather[0].icon;
+        weatherDiv.text(weatherValue);
+        $("#forcast").append(weatherDiv);
+
         var tempDiv = $("<div>").attr("id", "temp");
         var tempValue = data.main.temp;
         tempDiv.text("Temperature: " + tempValue);
         $("#forcast").append(tempDiv);
 
+        var humidityDiv = $("<div>").attr("id", "humidity");
+        var humidityValue = data.main.humidity;
+        humidityDiv.text("Humidity: " + humidityValue);
+        $("#forcast").append(humidityDiv);
+
+        var windDiv = $("<div>").attr("id", "wind");
+        var windValue = data.wind.speed;
+        windDiv.text("Wind Speed: " + windValue);
+        $("#forcast").append(windDiv);
+      
       })
       .catch(function (error) {
         console.error("Error fetching data:", error);
       });
     appendList(city);
   });
+      $("#searchBtn").on("click", function () {
+        var city = $("#city-search").val();
+        var AltQueryURL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&q=" + city + "&appid=" + APIKey + "&units=imperial"; 
+        fetch(AltQueryURL)
+          .then(function (response) {
+            if (!response.ok) {
+              throw new Error("Network response was not ok");
+            }
+            return response.json();
+          })
+          .then(function (data) {
+            console.log(data);
+
+
+        var mainDiv = $("<div>").attr("id", "main");
+        var mainValue = data.main;
+        mainDiv.text("mainerature: " + mainValue);
+        $("#fiveDayForecast").append(mainDiv);
+
+          
+          
+          });
+
+
+        })
+
 });
-console.log
+
