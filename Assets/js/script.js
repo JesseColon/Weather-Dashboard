1e8e57e6cf6ea74708a39b8c8e65f77e334bab40
JesseColon <jay.alexandervii@gmail.com>
2023-08-03 13:47:32 -0400

    edited comments/ added read me

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,7 @@
 $(function () {
   var currentCity;
+  var forcastArea = document.getElementById("forcast");
+  var fiveDayForcastArea = $("#fiveDayForecast");
   var APIKey = "5b7aef00fa1161dcd4e15780def2c64f";
 
   function appendList(city){
@@ -7,7 +9,16 @@ $(function () {
       .addClass("tile button is-info is-child notification level-item")
       .text(city);
     $("#aside").append(button);
+
+    button.on("click", function() {
+      currentCity = city;
+      forcastArea.empty(); 
+      fetchData(currentCity);
+    });
   } 
+ 
+   console.log(forcastArea);
+  var dataDiv = document.createElement("div");
 
   $("#searchBtn").on("click", function () {
     var city = $("#city-search").val();
@@ -21,41 +32,55 @@ $(function () {
       })
       .then(function (data) {
         console.log(data);
+
+        forcastArea.innerHTML = " ";
+
+        dataDiv.innerHTML = `
+          <h3>${data.name}</h3>
+          <img src="${data.weather[0].icon}" alt="${data.name}">
+          <p>${data.dt_text}</p>
+          <p>${data.main.temp}</p>
+          <p>${data.main.humidity}</p>
+          <p>${data.wind.speed}</p>
         
-        var nameH2 = $("<h2>").attr("id", "name").addClass("title");
-        var nameValue = data.name;
-        nameH2.text(nameValue);
-        $("#forcast").append(nameH2);
+        `;
+        forcastArea.appendChild(dataDiv);
 
-        var dt_textdiv = $("<div>").attr("id", "dt_text");
-        var dt_textValue = data.dt_text;
-        dt_textdiv.text(dt_textValue);
-        $("#forcast").append(dt_textdiv);
+        // var nameH2 = $("<h2>").attr("id", "name").addClass("title");
+        // var nameValue = data.name;
+        // nameH2.text(nameValue);
+        // $("#forcast").append(nameH2);
 
-        var weatherDiv = $("<div>").attr("id", "weather");
-        var weatherValue = data.weather[0].icon;
-        weatherDiv.text(weatherValue);
-        $("#forcast").append(weatherDiv);
+        // var dt_textdiv = $("<div>").attr("id", "dt_text");
+        // var dt_textValue = data.dt_text;
+        // dt_textdiv.text(dt_textValue);
+        // $("#forcast").append(dt_textdiv);
 
-        var tempDiv = $("<div>").attr("id", "temp");
-        var tempValue = data.main.temp;
-        tempDiv.text("Temperature: " + tempValue);
-        $("#forcast").append(tempDiv);
+        // var weatherimg = $("<img>").attr("id", "weather");
+        // var weatherValue = data.weather[0].icon;
+        // weatherimg.text(weatherValue);
+        // $("#forcast").append(weatherimg);
 
-        var humidityDiv = $("<div>").attr("id", "humidity");
-        var humidityValue = data.main.humidity;
-        humidityDiv.text("Humidity: " + humidityValue);
-        $("#forcast").append(humidityDiv);
+        // var tempDiv = $("<div>").attr("id", "temp");
+        // var tempValue = data.main.temp;
+        // tempDiv.text("Temperature: " + tempValue);
+        // $("#forcast").append(tempDiv);
 
-        var windDiv = $("<div>").attr("id", "wind");
-        var windValue = data.wind.speed;
-        windDiv.text("Wind Speed: " + windValue);
-        $("#forcast").append(windDiv);
+        // var humidityDiv = $("<div>").attr("id", "humidity");
+        // var humidityValue = data.main.humidity;
+        // humidityDiv.text("Humidity: " + humidityValue);
+        // $("#forcast").append(humidityDiv);
+
+        // var windDiv = $("<div>").attr("id", "wind");
+        // var windValue = data.wind.speed;
+        // windDiv.text("Wind Speed: " + windValue);
+        // $("#forcast").append(windDiv);
       
       })
       .catch(function (error) {
         console.error("Error fetching data:", error);
       });
+    fiveDayForcastArea.empty();  
     appendList(city);
   });
       $("#searchBtn").on("click", function () {
@@ -69,7 +94,7 @@ $(function () {
             return response.json();
           })
           .then(function (data) {
-            console.log(data);
+            console.log(data); 
 
 
         var mainDiv = $("<div>").attr("id", "main");
